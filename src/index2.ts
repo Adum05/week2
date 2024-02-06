c943d323fbeeab539b141cb1f005eaa0bbc84a18
Aday Munoz <amunozguedes@york.edu>
2024-02-05 20:02:48 -0600

    confirm

---
+++ src/index2.ts
@@ -0,0 +1,13 @@
+class Pilot{
+    team: string= '';
+    name: string= '';
+    dorsal: number= 0;
+
+    constructor(team: string, name: string, dorsal: number) {
+
+    }
+}
+
+let pilot1= new Pilot('Mercedes','Russell', 63)
+console.log('hi')
+
